356a626d8ac6e8c50bd4ff7d916b2986ba1298a1
Martín!!! <martin_panza_p@hotmail.com>
2015-10-08 01:33:09 -0300

    Se implementa funcion de van der pol

--- Tarea3.py
+++ Tarea3.py
@@ -5,3 +5,14 @@ Created on Tue Oct  6 19:29:14 2015
 Resolución de EDOs utilizando Runge Kutta
 """
 
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+u=1.534
+
+def funcion_osc_Van_Der_Pol(y,z): #y'=z
+    '''
+    Oscilador de Van der Pol reducido a dos EDOs
+    '''
+    return z, (-y) - (u* ((y**2)-1)*z)
